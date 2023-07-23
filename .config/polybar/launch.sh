7080d98bbfa7d19516cca243d8e90dfd91aaa090
Ross Bratton <bratton.ross@gmail.com>
2023-07-23 01:40:45 +0100

    add polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch
+polybar --config=$HOME/.config/polybar/config.ini brattonross 2>&1
